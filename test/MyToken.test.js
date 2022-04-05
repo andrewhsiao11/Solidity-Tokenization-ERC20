4e4cb1eca8baf5dd475d1c867940a4e2243ccc2f
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-04-05 14:10:11 -0400

    added Unit Tests for ERC20 token

---
+++ test/MyToken.test.js
@@ -0,0 +1,56 @@
+const Token = artifacts.require("MyToken");
+
+var chai = require("chai");
+
+//bring in Big Number Chai (see openzeppelin-test-helpers)
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+chai.use(chaiBN);
+
+//bring in chai as promised (see domenic/chai-as-promised)
+var chaiAsPromised = require("chai-as-promised");
+const { isTypedArray } = require("util/types");
+chai.use(chaiAsPromised);
+
+//using chai.expect rather than old way
+const expect = chai.expect;
+
+contract("Token Test", async (accounts) => {
+  const [initialHolder, recipient, anotherAccount] = accounts;
+    // TEST 1
+  it("All tokens should be in my account", async () => {
+    let instance = await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    // old way:
+    //let balance = await instance.balanceOf.call(initialHolder);
+    //assert.equal(balance.valueOf(), 0, "Account 1 has a balance");
+    //condensed, easier readable style:
+    await expect(
+      instance.balanceOf(initialHolder)
+    ).to.eventually.be.a.bignumber.equal(totalSupply);
+  });
+
+  //TEST 2
+  it("I can send tokens from Account 1 to Account 2", async () => {
+      const sendTokens = 1;
+      let instance = await Token.deployed();
+      let totalSupply = await instance.totalSupply();
+      // check initial holder has total supply
+      await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+      // check if transfer promise is fufilled
+      await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+      // check initial holders balance was decreased by amount
+      await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+      // check if recipient balance equals amount recieved
+      await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+  });
+
+  it("It's not possible to send more tokens than Account 1 has", async () => {
+      let instance = await Token.deployed();
+      let balanceOfAccount = await instance.balanceOf(initialHolder);
+      // confirm error if try to tranfer more than account balance
+      await expect(instance.transfer(recipient, new BN(balanceOfAccount + 1))).to.eventually.be.rejected;
+      // Check if balance is still the same
+      await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+  });
+});

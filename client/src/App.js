e371b18c4a9eb3d8d43f7bfbc58008bc106e5046
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-04-06 14:17:44 -0400

    frontend kyc functionality, metamask as owner config

--- client/src/App.js
+++ client/src/App.js
@@ -7,37 +7,37 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { loaded: false };
+  state = { loaded: false, kycAddress: "0x123..." };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-       this.web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-       this.accounts = await this.web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contracts' instances.
-      //  this.networkId = await this.web3.eth.net.getId(); --> doesn't work with metamask anymore
-      this.networkId = await this.web3.eth.getChainId();  
+      //  this.networkId = await this.web3.eth.net.getId(); // --> doesn't work with metamask anymore
+      this.networkId = await this.web3.eth.getChainId();
 
-       
-       this.myToken = new this.web3.eth.Contract(
-         MyToken.abi,
-         MyToken.networks[this.networkId] && MyToken.networks[this.networkId].address
-       );
-       
-       this.myTokenSale = new this.web3.eth.Contract(
-         MyTokenSale.abi,
-         MyTokenSale.networks[this.networkId] &&
-           MyTokenSale.networks[this.networkId].address
-       );
+      this.myToken = new this.web3.eth.Contract(
+        MyToken.abi,
+        MyToken.networks[this.networkId] &&
+          MyToken.networks[this.networkId].address
+      );
 
-       this.kycContract = new this.web3.eth.Contract(
-         KycContract.abi,
-         KycContract.networks[this.networkId] &&
-           KycContract.networks[this.networkId].address
-       );
+      this.myTokenSale = new this.web3.eth.Contract(
+        MyTokenSale.abi,
+        MyTokenSale.networks[this.networkId] &&
+          MyTokenSale.networks[this.networkId].address
+      );
+
+      this.kycContract = new this.web3.eth.Contract(
+        KycContract.abi,
+        KycContract.networks[this.networkId] &&
+          KycContract.networks[this.networkId].address
+      );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
@@ -45,10 +45,24 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleKycSubmit = async () => {
+    const { kycAddress } = this.state;
+    await this.kycContract.methods
+      .setKycCompleted(kycAddress)
+      .send({ from: this.accounts[0] });
+    alert("Account " + kycAddress + " is now whitelisted");
   };
 
   render() {
@@ -57,17 +71,29 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>TravisCoin Token Sale</h1>
+        {/* <p>Token Sale</p> */}
+        <h2>Enable your account</h2>
+
+        <div className="container input-group justify-content-md-center">
+          <label className="form-label input-group-text">
+            Address to allow:
+            <input
+              type="text"
+              name="kycAddress"
+              value={this.state.kycAddress}
+              onChange={this.handleInputChange}
+              className="form-control"
+            />
+            <button
+              type="button"
+              onClick={this.handleKycSubmit}
+              className="btn btn-primary"
+            >
+              Add Address to Whitelist
+            </button>
+          </label>
+        </div>
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({path: './.env'});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,8 +9,19 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545,
+      port: 8545, 
     },
+    development: {
+      port: 7545,
+      network_id: "1337",
+      host: "127.0.0.1",
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex)
+      },
+      network_id: 1337
+    }
   },
   compilers: {
     solc: {
